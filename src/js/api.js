59a4cdef9cb4808dc8f881882074ad6395dfe539
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-30 11:50:06 -0400

    updated API to https

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
 export const getWeatherData = async (location = 'La Vega') => {
     try {
-        let res = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=baf91d611713492c854134715203012&q=${location}`, {
+        let res = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=baf91d611713492c854134715203012&q=${location}`, {
             mode: 'cors',
         })
         if (res.status === 400) throw new Error('Not found');
